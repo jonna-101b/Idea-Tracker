ab9586422e57a4e64cf9ca6fcca3e0bb9f0d385c
Jonna <yonazyonaz8@gmail.com>
2026-08-05 20:58:37 +0300

    Minor fix

--- backend/routes/authRouter.ts
+++ backend/routes/authRouter.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import { signupUser, loginUser } from "../controllers/authController.js";
 import { loginValidator, signupValidator } from "../validators/user.validator.js";
-import { authenticate } from "../middlewares/authenticate.js";
 
 const router = express.Router();
 
-router.post("/signup", signupValidator, authenticate, signupUser);
-router.post("/login", loginValidator, authenticate, loginUser);
+router.post("/signup", signupValidator, signupUser);
+router.post("/login", loginValidator, loginUser);
 
 export default router;
